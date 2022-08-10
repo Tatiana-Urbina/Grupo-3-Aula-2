da49445fcad493e4427bf08ad2aefab3f5adbb1e
Gonzalo <89819623+GonzaC21@users.noreply.github.com>
2022-08-10 20:14:16 -0300

    Valoración Final Primer Cuatrimestre
    Introducción a la programación

---
+++ Consigna 1 ejercicio 10.py
@@ -0,0 +1,7 @@
+""" lista denominada “Clientes” con los nombres de los diferentes  dueños de perros
+ y Ordenada alfabéticamente y mostrada por pantalla. """
+
+
+Clientes = ['Juan', 'Mario', 'Ariel', 'Josefina','Marianella']
+Clientes.sort()
+print(Clientes)
